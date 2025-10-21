34b824845e3b999ac71b9b14b3465aea4b35a967
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-10-21 20:53:21 +0100

    Add meta class for post

--- blog/admin.py
+++ blog/admin.py
@@ -5,3 +5,11 @@ from .models import Post, Comment
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
+
+
+class Post(models.Model):
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
