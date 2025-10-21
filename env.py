80810be5c49139c421e1a8a01a99ac9fe579e669
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-10-21 20:17:50 +0100

    Configure database for deployment

--- env.py
+++ env.py
@@ -4,3 +4,4 @@ os.environ.setdefault(
     "DATABASE_URL",
     "postgresql://neondb_owner:npg_Z3FiHGoV0dsh@ep-old-sound-agr48bp4.c-2.eu-central-1.aws.neon.tech/ozone_dock_tummy_982427",
 )
+os.environ.setdefault("SECRET_KEY", "banana")
