83675c7329121ae59b6b868fd7a409bc2a8325e7
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-10-21 20:47:28 +0100

    Amend dabase link and add to comment model

--- env.py
+++ env.py
@@ -2,6 +2,6 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL",
-    "postgresql://neondb_owner:npg_Z3FiHGoV0dsh@ep-old-sound-agr48bp4.c-2.eu-central-1.aws.neon.tech/ozone_dock_tummy_982427",
+    "postgresql://neondb_owner:npg_Z3FiHGoV0dsh@ep-old-sound-agr48bp4.c-2.eu-central-1.aws.neon.tech/late_clash_lyric_564715",
 )
 os.environ.setdefault("SECRET_KEY", "banana")
