e29a603e6de82f1db0d8e0088cc54c3911f85fd1
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-10-21 20:13:01 +0100

    Change debug status for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8hl#pv^%4g)sc-gmu+_j*!143=a^g^_jixuk$#wiq(=ns+vul_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
