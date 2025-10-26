651fef52245157d89b28d67fcad8bd4c5fa72ca4
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-10-26 13:22:32 +0000

    Update delete function for comments

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
         views.comment_edit,
         name="comment_edit",
     ),
+    path(
+        "<slug:slug>/delete_comment/<int:comment_id>",
+        views.comment_delete,
+        name="comment_delete",
+    ),
 ]
